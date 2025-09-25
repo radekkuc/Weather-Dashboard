0f3944ad5e4d9d59bc62e5b95cfdae1d86caff74
radekkuc <radekkuc5@gmail.com>
2025-09-25 14:35:35 +0200

    Started with controller, consider changing name of entity

--- ProfileService/src/main/java/com/example/ProfileService/profile/CityController.java
+++ ProfileService/src/main/java/com/example/ProfileService/profile/CityController.java
@@ -1,8 +1,19 @@
 package com.example.ProfileService.profile;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CityController {
+
+    @GetMapping("/favourite")
+    ResponseEntity<List<City>> getFavouriteCities(City city){
+
+    }
+
+
 
 }
