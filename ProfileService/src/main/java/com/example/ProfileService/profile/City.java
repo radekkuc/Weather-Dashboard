3b098b5db9c550b762b3651fff3be0cab8d964be
radekkuc <radekkuc5@gmail.com>
2025-09-25 12:34:26 +0200

    Entity created, table created

--- ProfileService/src/main/java/com/example/ProfileService/profile/City.java
+++ ProfileService/src/main/java/com/example/ProfileService/profile/City.java
@@ -1,14 +1,45 @@
 package com.example.ProfileService.profile;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class City {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private Long userId;
     private String name;
 
+    public City(Long userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
