1833c9c089b11df32db121c714c994f1ae79604d
radekkuc <radekkuc5@gmail.com>
2025-10-26 15:47:41 +0100

    Config started, finish it and take into account that there can be users or admins

--- AuthService/src/test/java/com/example/AuthService/authorization/SecurityConfig.java
+++ AuthService/src/test/java/com/example/AuthService/authorization/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.AuthService.authorization;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +13,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
+                .sessionManagement(sess -> sess
+                        // Jwt requires stateless session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
 
     }
     // Without it, basic filter chain is implemented anyway but without this we would need to log before
