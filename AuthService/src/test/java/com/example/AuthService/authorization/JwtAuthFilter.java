bceab07e40a08e1c9bd43d206a51983fbc4af8b0
radekkuc <radekkuc5@gmail.com>
2025-10-27 16:34:44 +0100

    JwtConfig finished, move to authenticator

---
+++ AuthService/src/test/java/com/example/AuthService/authorization/JwtAuthFilter.java
@@ -0,0 +1,17 @@
+package com.example.AuthService.authorization;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// Must extend it to be treated as filter
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
--- AuthService/src/test/java/com/example/AuthService/authorization/SecurityConfig.java
+++ AuthService/src/test/java/com/example/AuthService/authorization/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.AuthService.authorization;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +8,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthFilter filter) {
+        this.jwtAuthFilter = filter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,11 +28,13 @@ public class SecurityConfig {
                 .sessionManagement(sess -> sess
                         // Jwt requires stateless session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
     // Without it, basic filter chain is implemented anyway but without this we would need to log before
-    // every request and tokens would not work
+    // every request and tokens would not work, UsernamePassword class is ready, no need to define it
+    // yourself
 
     @Bean
     public PasswordEncoder passwordEncoder() {
