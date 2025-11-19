046c203550b06bd58353208040ae84f3bf18ffd2
radekkuc <radekkuc5@gmail.com>
2025-11-19 21:26:45 +0100

    Started implementing security to profile, still in progress

--- ProfileService/src/main/java/com/example/ProfileService/profile/authorization/TokenUserService.java
+++ ProfileService/src/main/java/com/example/ProfileService/profile/authorization/TokenUserService.java
@@ -1,15 +1,19 @@
 package com.example.ProfileService.profile.authorization;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TokenUserDetailsService implements UserDetailsService {
+public class TokenUserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return User.withUsername(username)
+                .password("")
+                .authorities("User")
+                .build();
     }
 }
